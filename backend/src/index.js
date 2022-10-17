4188b7f3d336a2c872755085af7a11b3b1b18372
nicobolton2 <nicoboltongithub@gmail.com>
2022-10-17 18:59:26 -0300

    4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
-const app = require('./app');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
-async function main(){
+//setting
+app.set('port',process.env.PORT || 3000);
 
-    await app.listen(3000);
-    console.log("server is running");
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-}
+//routes
+app.use(require('./routes/index'));
 
-main();
+//starting the server
+app.listen(3000,() =>{
+    console.log(`Server on port ${app.get('port')}`);
+});
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,45 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
+//conection bdds
+//const pool = require("../../db");
 
-router.get('/',(req,res) => res.json({menssage: 'Hello Word!!'}));
+
+//home
+router.get('/', (req, res) => {
+    res.send({'title':'/'});
+});
+router.get('/home', async(req, res) => {
+    res.send({'title':'home'});
+});
+
+
+//USER
+router.post('/register', async(req, res) => {
+    //const {user, pass}=req.body;
+    res.send({'title':'register'});
+});
+router.post('/login', async(req, res) => {
+    //const {user, pass}=req.body;
+    res.send({'title':'login'});
+});
+
+
+//CRUD para las reseñas
+//CREATE - crear
+router.post('/create', async(req, res) => {
+    res.send({'title':'create'});
+});
+//READ - leer
+router.post('/read', async(req, res) => {
+    res.send({'title':'read'});
+});
+//UPGRADE - cambiar/mejorar/editar
+router.put('/upgrade', async(req, res) => {
+    res.send({'title':'upgrade'});
+});
+//DELETE - borrar
+router.delete('/delete', async(req, res) => {
+    res.send({'title':'delete'});
+});
 
 module.exports = router;
